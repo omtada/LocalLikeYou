cc3c8e84d944345c26ccd5bcf89b5af4c8a9b9a1
Om Tada <Om Tada@DESKTOP-DLT0R3R>
2023-10-18 11:19:21 -0700

    Add project files.

---
+++ LocalLikeYou/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LocalLikeYou
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LocalLikeYou/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalLikeYou
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
